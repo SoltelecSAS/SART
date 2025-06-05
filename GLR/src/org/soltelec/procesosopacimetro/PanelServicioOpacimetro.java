711c3e060a20d3ea602da412cc00c36bac522b39
Oagomezr <98911168+Oagomezr@users.noreply.github.com>
2025-06-05 11:21:33 -0500

    correccion opacimetro brian be part2

--- GLR/src/org/soltelec/procesosopacimetro/PanelServicioOpacimetro.java
+++ GLR/src/org/soltelec/procesosopacimetro/PanelServicioOpacimetro.java
@@ -103,6 +103,7 @@ public class PanelServicioOpacimetro extends JPanel {
 
         } catch (IOException ex) {
         }
+        marcaOpacimetro = "BrianBee";
 
         if (marcaOpacimetro == null || marcaOpacimetro.equals("Sensors")) {
             opacimetro = new OpacimetroSensors();
